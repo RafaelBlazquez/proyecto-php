d8e572e72ec8c5718954f570706ec9f909c0edda
RafaelBlazquez <rafael.blazquez@ikasle.egibide.org>
2021-10-19 13:46:19 +0200

    Confirmo1

--- ej/Ejercicio12.php
+++ ej/Ejercicio12.php
@@ -11,8 +11,6 @@ function setValor($array,$posicion,$valor){
 
 $resultado1 = getValor($ciudades,1);
 
-setValor($ciudades,1,"Vitoria");
-
 $resultado2 = getValor($ciudades,1);
 
 require "Ejercicio12.view.php";
